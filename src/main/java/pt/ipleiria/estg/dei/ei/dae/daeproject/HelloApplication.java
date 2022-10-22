f218a34c341cf4be9ef8d2377db7420b85ac1d10
Alexandre <alexandrejeronimo2001@gmail.com>
2022-10-23 00:49:46 +0100

    javax

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/daeproject/HelloApplication.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/daeproject/HelloApplication.java
@@ -1,7 +1,8 @@
 package pt.ipleiria.estg.dei.ei.dae.daeproject;
 
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
