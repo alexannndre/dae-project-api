ea44d5f75f8747f6a3befb7025248ad94ce41f9b
tgn <trwygon@outlook.pt>
2023-01-11 22:17:42 +0000

    changed policy manager logic

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/daeprojectapi/managers/PolicyManager.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/daeprojectapi/managers/PolicyManager.java
@@ -8,43 +8,43 @@ import pt.ipleiria.estg.dei.ei.dae.daeprojectapi.pojos.Policy;
 
 
 import javax.ejb.EJB;
+import javax.ejb.Singleton;
 import javax.json.Json;
 import javax.json.JsonReader;
+import javax.json.JsonValue;
 import java.io.StringReader;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
+@Singleton
 public class PolicyManager {
 
     @EJB
-    CustomerBean customerBean = new CustomerBean();
+    private static CustomerBean customerBean = new CustomerBean();
 
-    private final String API_URL = "https://63beea09585bedcb36ba824f.mockapi.io/api";
+    private static final String API_URL = "https://63beea09585bedcb36ba824f.mockapi.io/api";
 
-    public List<Policy> policies;
+    private static void processJsonValue(List<Policy> policies, JsonValue jsonValue) {
+        var jsonObject = jsonValue.asJsonObject();
+        var policy = new Policy();
 
-    public PolicyManager() {
-        this.policies = new LinkedList<>();
+        policy.setCustomer(customerBean.findOrFail(jsonObject.getString("customer_vat")));
+        policy.setCode(jsonObject.getString("code"));
+        policy.setCode(jsonObject.getString("code"));
+        policy.setInsurerCompany(jsonObject.getString("insurer_company"));
+        policy.setType(jsonObject.getString("type"));
+        jsonObject.getJsonArray("covers").forEach(coverJsonValue -> policy.addCover(coverJsonValue.toString()));
+
+        policies.add(policy);
     }
 
-    public List<Policy> getPolicies() {
-        return policies;
-    }
+    private static List<Policy> retrievePolicies(String args){
+        List<Policy> policies = new LinkedList<>();
 
-    private void addPolicy(Policy policy){
-        if(!policies.contains(policy))
-            policies.add(policy);
-    }
-
-    private void removePolicy(Policy policy){
-        policies.remove(policy);
-    }
-
-    public void retrievePolicies(){
         OkHttpClient client = new OkHttpClient();
 
-        var url = String.format("%s/policies",API_URL);
+        var url = String.format("%s/policies?%s",API_URL,args);
 
         var request = new Request.Builder()
                 .url(url)
@@ -54,20 +54,18 @@ public class PolicyManager {
             var jsonStr = Objects.requireNonNull(response.body()).string();
             //Deserialize json
             JsonReader jsonReader = Json.createReader(new StringReader(jsonStr));
-            jsonReader.readArray().forEach(jsonValue -> {
-                var jsonObject = jsonValue.asJsonObject();
-                var policy = new Policy();
-                policy.setCustomer(customerBean.findOrFail(jsonObject.getString("customer_vat")));
-                policy.setCode(jsonObject.getString("code"));
-                policy.setCode(jsonObject.getString("code"));
-                policy.setInsurerCompany(jsonObject.getString("insurer_company"));
-                policy.setType(jsonObject.getString("type"));
-                jsonObject.getJsonArray("covers").forEach(coverJsonValue -> policy.addCover(coverJsonValue.toString()));
-
-                addPolicy(policy);
-            });
+            jsonReader.readArray().forEach(jsonValue -> processJsonValue(policies, jsonValue));
         }catch (Exception e) {
             e.printStackTrace();
         }
+        return policies;
+    }
+
+    public static List<Policy> getAllPolicies(){
+        return retrievePolicies("");
+    }
+
+    public static List<Policy> getPoliciesByVat(Object vat){
+        return retrievePolicies("customer_vat="+vat.toString());
     }
 }
