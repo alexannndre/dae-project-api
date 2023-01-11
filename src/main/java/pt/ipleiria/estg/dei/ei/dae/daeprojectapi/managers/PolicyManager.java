3b2406f5c3566a456153e87d0a98e19511662459
tgn <trwygon@outlook.pt>
2023-01-11 19:24:19 +0000

    retrievePolicies()

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/daeprojectapi/managers/PolicyManager.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/daeprojectapi/managers/PolicyManager.java
@@ -1,12 +1,20 @@
 package pt.ipleiria.estg.dei.ei.dae.daeprojectapi.managers;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import pt.ipleiria.estg.dei.ei.dae.daeprojectapi.pojos.Policy;
 
 
+import javax.json.Json;
+import javax.json.JsonReader;
+import java.io.StringReader;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class PolicyManager {
+
+    private final String API_URL = "https://63beea09585bedcb36ba824f.mockapi.io/api";
 
     public List<Policy> policies;
 
@@ -18,12 +26,41 @@ public class PolicyManager {
         return policies;
     }
 
-    public void addPolicy(Policy policy){
+    private void addPolicy(Policy policy){
         if(!policies.contains(policy))
             policies.add(policy);
     }
 
-    public void removePolicy(Policy policy){
+    private void removePolicy(Policy policy){
         policies.remove(policy);
+    }
+
+    public void retrievePolicies(){
+        OkHttpClient client = new OkHttpClient();
+
+        var url = String.format("%s/policies",API_URL);
+
+        var request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (var response = client.newCall(request).execute()) {
+            var jsonStr = Objects.requireNonNull(response.body()).string();
+            //Deserialize json
+            JsonReader jsonReader = Json.createReader(new StringReader(jsonStr));
+            jsonReader.readArray().forEach(jsonValue -> {
+                var jsonObject = jsonValue.asJsonObject();
+                var policy = new Policy();
+
+                policy.setCode(jsonObject.getString("code"));
+                policy.setInsurerCompany(jsonObject.getString("insurerCompany"));
+                policy.setType(jsonObject.getString("type"));
+                jsonObject.getJsonArray("covers").forEach(coverJsonValue -> policy.addCover(coverJsonValue.toString()));
+
+                addPolicy(policy);
+            });
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
