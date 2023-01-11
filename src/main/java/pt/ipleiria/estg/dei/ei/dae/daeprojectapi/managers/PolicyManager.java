15c6626f1afd9a1e690fc06ee1d313b7527895ab
tgn <trwygon@outlook.pt>
2023-01-11 23:34:59 +0000

    so digo gg so vim dizer gg mm

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/daeprojectapi/managers/PolicyManager.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/daeprojectapi/managers/PolicyManager.java
@@ -20,16 +20,13 @@ import java.util.Objects;
 @Singleton
 public class PolicyManager {
 
-    @EJB
-    private static CustomerBean customerBean = new CustomerBean();
-
     private static final String API_URL = "https://63beea09585bedcb36ba824f.mockapi.io/api";
 
-    private static void processJsonValue(List<Policy> policies, JsonValue jsonValue) {
+    private static void processJsonValue(List<Policy> policies, CustomerBean cb, JsonValue jsonValue) {
         var jsonObject = jsonValue.asJsonObject();
         var policy = new Policy();
         System.out.println(jsonObject.getString("customer_vat"));
-        policy.setCustomer(customerBean.findOrFail(jsonObject.getString("customer_vat")));
+        policy.setCustomer(cb.findOrFail(jsonObject.getString("customer_vat")));
         policy.setCode(jsonObject.getString("code"));
         policy.setCode(jsonObject.getString("code"));
         policy.setInsurerCompany(jsonObject.getString("insurer_company"));
@@ -39,7 +36,7 @@ public class PolicyManager {
         policies.add(policy);
     }
 
-    private static List<Policy> retrievePolicies(String args){
+    private static List<Policy> retrievePolicies(String args, CustomerBean cb){
         List<Policy> policies = new LinkedList<>();
 
         OkHttpClient client = new OkHttpClient();
@@ -54,20 +51,20 @@ public class PolicyManager {
             var jsonStr = Objects.requireNonNull(response.body()).string();
             //Deserialize json
             JsonReader jsonReader = Json.createReader(new StringReader(jsonStr));
-            jsonReader.readArray().forEach(jsonValue -> processJsonValue(policies, jsonValue));
+            jsonReader.readArray().forEach(jsonValue -> processJsonValue(policies, cb, jsonValue));
         }catch (Exception e) {
             e.printStackTrace();
         }
         return policies;
     }
 
-    public static List<Policy> getAllPolicies(){
-        return retrievePolicies("");
+    public static List<Policy> getAllPolicies(CustomerBean cb){
+        return retrievePolicies("", cb);
     }
 
-    public static List<Policy> getPoliciesByVat(String vat){
-        return retrievePolicies("customer_vat="+vat);
+    public static List<Policy> getPoliciesByVat(CustomerBean cb, String vat){
+        return retrievePolicies("customer_vat="+vat, cb);
     }
 
-    public static List<Policy> getPoliciesByCode(String code){ return retrievePolicies("code="+code); }
+    public static List<Policy> getPoliciesByCode(CustomerBean cb, String code){ return retrievePolicies("code="+code, cb); }
 }
